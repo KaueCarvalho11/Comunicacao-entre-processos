e5740f80857d5db4391033d88bfaa8d6f158a0fa
Jean-Richardson <jeanrichardson88@gmail.com>
2025-06-15 13:36:55 -0300

    Realizar envio de mensagens entre cliente e servidor

--- src/Cliente.java
+++ src/Cliente.java
@@ -17,10 +17,31 @@ public class Cliente {
             Socket cliente = new Socket(host, porta);
             System.out.println("Estabelecendo conexão com a porta: " + porta);
 
+            PrintWriter saida = new PrintWriter(cliente.getOutputStream(), true);
+
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+
+            saida.println("Obrigado por me deixar entrar");
+
+            System.out.println("Resposta do servidor: " + entrada.readLine());
+            ;
+
+            cliente.close();
+            saida.close();
+            entrada.close();
+
         } catch (IOException e) {
             System.out.println(e.getMessage());
             System.out.println("Erro ao estabelecer conexão");
         }
+
+    }
+
+    public static void main(String[] args) {
+
+        Cliente c1 = new Cliente();
+
+        c1.inicialiaCliente();
 
     }
 
